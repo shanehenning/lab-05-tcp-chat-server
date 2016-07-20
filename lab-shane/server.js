74a4e6f946d5dc2ca4714d781b25cd91674f5690
Shane Henning <shanerhenning@gmail.com>
2016-07-19 18:20:04 -0700

    attaching to constructor

--- lab-shane/server.js
+++ lab-shane/server.js
@@ -3,59 +3,58 @@
 const net = require('net');
 const EE = require('events');
 
-var ee = new EE();
-var count = 1;
-var thisUser;
-
 var ClientPool = function(){
   this.pool = {};
+  this.ee = new EE();
+
+  this.ee.on('register', function(socket){
+    socket.write('hello from server!\n');
+    socket.pipe(process.stdout);
+    socket.id = socket.remotePort;
+    socket.userName = 'user-' + socket.id;
+    this.pool[socket.id] = socket.userName;
+  }.bind(this));
+
+  this.ee.on('broadcast', function(data, socket){
+    Object.keys(this.pool).forEach(function(item){
+      clients[item].write(socket);
+    });
+    // console.log(this.pool);
+
+
+    // for (var client in this.pool){
+    //   console.log(this.pool[client] + ' msg: ' + data.toString());
+    // }
+  }.bind(this));
+
 };
+
+
+
+
+
 var clients = new ClientPool();
 
-function addUser(){
-  var newUser = 'user ' + count;
-  var createdUser = clients.pool[count] = newUser;
-  count++;
-  return createdUser;
-}
-
-ee.on('register', function(){
-  thisUser = count;
-  addUser();
-  console.log('user ' + thisUser + ' connected');
-  console.log('clients pool: ' + clients.pool);
-});
-
-ee.on('broadcast', function(socket){
-  // var clientsArray = Object.keys(clients.pool);
-  // clientsArray.forEach(function(client){
-  //   client.pipe(socket);
-  //   socket.pipe(client);
-  // });
-});
-
 let server = net.createServer(function(socket){
-  ee.emit('register');
-  socket.write('hello from the server\n');
-  socket.pipe(process.stdout);
+  clients.ee.emit('register', socket);
 
   socket.on('data', function(data){
     if(data.toString() === 'END\r\n'){
       socket.end();
+      delete clients.pool[socket.id];
+    } else {
+      // console.log('clients.pool[socket.id] = ' + clients.pool[socket.id]);
+      clients.ee.emit('broadcast', data, socket);
     }
-    ee.emit('broadcast');
-    socket.write('user ' + thisUser + ' msg: ' + data.toString());
   });
 
   socket.on('error', function(err){
     if(err) console.log(err);
   });
 
-
   socket.on('close', function(){
-    console.log('user ' + thisUser + ' disconnected');
-    delete clients.pool[thisUser];
-    console.log('clients pool: ' + clients.pool);
+    console.log(clients.pool[socket.id] + ' disconnected');
+    delete clients.pool[socket.id];
   });
 });
 
